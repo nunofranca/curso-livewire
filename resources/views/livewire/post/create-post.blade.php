8c6bee630e3ef5578698712951500f74d8fa3448
Nuno Franca <contato@nunotech.com>
2023-07-17 02:40:27 -0300

    feat - Iniciado creação do CreatePost

---
+++ resources/views/livewire/post/create-post.blade.php
@@ -0,0 +1,30 @@
+<div>
+    <!-- Button trigger modal -->
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        NOVO POST
+    </button>
+
+    <!-- Modal -->
+    <div class="modal modal-lg fade" data-bs-backdrop="static" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog  modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">NOVO POST</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    ...
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <span>
+
+    </span>
+
+</div>
+
